81c66226c7e4e624974e5d320368c040334b96e5
Антон Ткачук <chiga2030@gmail.com>
2020-12-05 00:36:57 +0300

    rename script file

--- source/scripts/01-4-addBackside.js
+++ source/scripts/01-4-addBackside.js
@@ -1,11 +1,12 @@
 function addBackside(event){
   const curentEl = event.target.classList.value;
   const nextEl = event.target.nextElementSibling;
-  const modifier = bugGenerator();
   const parentEl = event.toElement.parentElement;
+  const modifier = bugGenerator();
 
   if (curentEl === 'play-ground__card-frontside') {
     nextEl.classList.add(`play-ground__card-backside_${modifier}`);
     flipCard(parentEl);
+    // nextEl.addEventlistener('click', restartGame, false)
   }
 }
