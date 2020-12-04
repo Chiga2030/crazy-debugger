75a5bbe66764edc805e5f59459a07829ce66f577
Антон Ткачук <chiga2030@gmail.com>
2020-12-05 00:35:30 +0300

    search layers in document

---
+++ source/scripts/00-1-variables.js
@@ -0,0 +1,6 @@
+const layers = {
+  'start': searchEl('.select-level'),
+  'easy': searchEl('.play-ground_easy'),
+  'normal': searchEl('.play-ground_normal'),
+  'hard': searchEl('.play-ground_hard'),
+}
